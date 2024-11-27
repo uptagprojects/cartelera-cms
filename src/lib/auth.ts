3875a3b30f272e483fba1db243fe02a62bb42f18
Jhinel Arcaya <hello@ginnie.codes>
2024-11-27 08:06:44 -0400

    ci: temp access for presentational purposes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,6 @@ import Resend from "next-auth/providers/resend";
 
 import { authConfig } from "../../auth.config";
 import postgresAdapter from "../contexts/cma/auth/infrastructure/PostgresAuthAdapter";
-import { UserProviderConfirmer } from "../contexts/cma/users/application/confirm-from-provider/UserProviderConfirmer";
-import { UserEmailFinder } from "../contexts/cma/users/application/find-by-email/UserEmailFinder";
-import { PostgresUserRepository } from "../contexts/cma/users/infrastructure/PostgresUserRepository";
 import { OfficialUuidGenerator } from "../contexts/shared/infrastructure/OfficialUuidGenerator";
 import { PostgresConnection } from "../contexts/shared/infrastructure/PostgresConnection";
 
@@ -33,29 +30,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 			return token;
 		},
 
-		async signIn({ user, profile }) {
-			if (user.email === "dibarrosivo@gmail.com") return true;
-			if (!user.email) return false;
-
-			const connection = new PostgresConnection();
-			const repository = new PostgresUserRepository(connection);
-
-			const savedUser = await new UserEmailFinder(repository).find(user.email);
-			const { id, status } = savedUser;
-
-			if (status === "pending_confirmation") {
-				await connection.transactional(async () => {
-					await new UserProviderConfirmer(repository).confirm(
-						id,
-						profile?.name ?? undefined,
-						profile?.picture
-					);
-				});
-
-				return true;
-			}
-
-			return status === "active";
+		async signIn() {
+			return true;
 		}
 	}
 });
